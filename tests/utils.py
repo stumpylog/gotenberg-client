415b00d1bd1cabed91cb594ecca0807c9feaeabf
Trenton H <797416+stumpylog@users.noreply.github.com>
2025-10-09 07:19:27 -0700

    Chore: Use a pure Python PDF reader in unit testing (#75)

--- tests/utils.py
+++ tests/utils.py
@@ -1,12 +1,12 @@
 # SPDX-FileCopyrightText: 2023-present Trenton H <rda0128ou@mozmail.com>
 #
 # SPDX-License-Identifier: MPL-2.0
-import shutil
-import subprocess
-import tempfile
+import re
 from http import HTTPStatus
 from pathlib import Path
 from typing import Union
+
+from pypdf import PdfReader
 
 from gotenberg_client.responses import SingleFileResponse
 from gotenberg_client.responses import ZipFileResponse
@@ -30,26 +30,10 @@ def verify_stream_contains(request, key: str, value: str) -> None:
     assert form_field_found, f'Key "{key}" with value "{value}" not found in stream'
 
 
-def extract_text(pdf_path: Path) -> str:
+def extract_text(pdf_path: Path) -> list[str]:
     """
-    Using pdftotext from poppler, extracts the text of a PDF into a file,
-    then reads the file contents and returns it
+    Extracts text from a PDF and returns it as a list of lines.
+    All whitespace (tabs, multiple spaces) is normalized to single spaces.
     """
-    pdf_to_text = shutil.which("pdftotext")
-    assert pdf_to_text is not None
-    with tempfile.NamedTemporaryFile(
-        mode="w+",
-    ) as tmp:
-        subprocess.run(
-            [
-                pdf_to_text,
-                "-q",
-                "-layout",
-                "-enc",
-                "UTF-8",
-                str(pdf_path),
-                tmp.name,
-            ],
-            check=True,
-        )
-        return tmp.read()
+    with PdfReader(pdf_path) as reader:
+        return [re.sub(r"\s+", " ", line) for page in reader.pages for line in page.extract_text().splitlines()]
